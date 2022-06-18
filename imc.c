0b28b6538a45f29fd9d2b872638b1403afd44a3c
rogerioBlankemburg <roger.front79@gmail.com>
2022-06-18 14:27:21 -0300

    imc

---
+++ imc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float peso, alt, imc;
+
+    printf("Digite o seu peso: ");
+    scanf("%f", &peso);
+    printf("Digite sua altura: ");
+    scanf("%f", &alt);
+
+    imc = peso/(alt * alt);
+
+    printf("\nIMC: %.1f", imc);
+
+}
